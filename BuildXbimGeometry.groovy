ffc8c6a91bb0adb327b939f5779d90b15412d0ac
Bernold Kraft <bernold.kraft@inros-lackner.de>
2019-07-29 09:04:41 +0200

    Fixing fileversion replacement of dlls

--- BuildXbimGeometry.groovy
+++ BuildXbimGeometry.groovy
@@ -38,14 +38,14 @@ node {
           XbimStages.nuget('update')
       }
       // Replace versions
-      powershell "((Get-Content -path Xbim.Geometry.Engine/app.rc -Raw) -replace '\"FileVersion\", \"5.0.0.0\"','\"FileVersion\", \"${packageVersion})\"') | Set-Content -Path Xbim.Geometry.Engine/app.rc" 
-      powershell "((Get-Content -path Xbim.Geometry.Engine/app.rc -Raw) -replace 'FILEVERSION 5,0,0,0','FILEVERSION ${buildVersion.major},${buildVersion.minor},${buildVersion.release},${buildVersion.build}') | Set-Content -Path Xbim.Geometry.Engine/app.rc" 
+      powershell "((Get-Content -path Xbim.Geometry.Engine\\app.rc -Raw) -replace '"FileVersion", "${buildVersion.major}.${buildVersion.minor}.0.0"','"FileVersion", "${packageVersion}"') | Set-Content -Path Xbim.Geometry.Engine\\app.rc" 
+      powershell "((Get-Content -path Xbim.Geometry.Engine\\app.rc -Raw) -replace 'FILEVERSION ${buildVersion.major},${buildVersion.minor},0,0','FILEVERSION ${buildVersion.major},${buildVersion.minor},${buildVersion.release},${buildVersion.build}') | Set-Content -Path Xbim.Geometry.Engine\\app.rc" 
    }
 
    stage('Build') {
        // Build Engine for x86 and x64 mode
-       XbimStages.msbuild("./Xbim.Geometry.Engine/Xbim.Geometry.Engine.vcxproj /t:build /p:Configuration=${buildConfig} /p:Platform=x64")
-       XbimStages.msbuild("./Xbim.Geometry.Engine/Xbim.Geometry.Engine.vcxproj /t:build /p:Configuration=${buildConfig} /p:Platform=x86")
+       XbimStages.msbuild("./Xbim.Geometry.Engine.sln /t:build /p:Configuration=${buildConfig} /p:Platform=x64")
+       XbimStages.msbuild("./Xbim.Geometry.Engine.sln /t:build /p:Configuration=${buildConfig} /p:Platform=x86")
        // Pack nuget packages
        powershell "dotnet pack Xbim.Geometry.Engine.Interop/Xbim.Geometry.Engine.Interop.csproj -c ${buildConfig} -o Xbim.Geometry.Engine.Interop/bin/${buildConfig} /p:PackageVersion=${packageVersion}"
        powershell "dotnet pack Xbim.ModelGeometry.Scene/Xbim.ModelGeometry.Scene.csproj -c ${buildConfig} -o Xbim.ModelGeometry.Scene/bin/${buildConfig} /p:PackageVersion=${packageVersion}"
